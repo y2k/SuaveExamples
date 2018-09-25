c5d55019b158c2c01b2ed9845385ee10d42d5471
y2k <itwisterlx@gmail.com>
2018-09-25 21:21:26 +0300

    write and read cookies

--- Program.fs
+++ Program.fs
@@ -1,30 +1,43 @@
 ﻿open System
+open System.Text
 open Suave
 open Suave.Operators
 open Suave.Filters
 open Suave.Successful
 open Suave.Cookie
 
+let private cookieState =
+    CookiesState.create
+        (Encoding.ASCII.GetBytes "01234567890123456789012345678901")
+        "userId"
+        "userId"
+        (MaxAge <| TimeSpan.FromDays 1.)
+        false
 
 type Snapshot = { id : int }
 type SnapshotsResult = { items : Snapshot list }
 
-let foo =
-    { items = [ { id = 1 }; { id = 2 }; { id = 3 } ] }
-    |> string
-    |> OK
+let login (email : string) =
+    Cookie.updateCookies
+        cookieState
+        (fun _ -> Encoding.UTF8.GetBytes email)
 
-let foo2 =
-    Authentication.authenticated (MaxAge <| TimeSpan.FromDays 1.) true
-
-let foo3 =
-    Authentication.authenticateWithLogin
-        (MaxAge <| TimeSpan.FromDays 1.)
-        "/login"
-        foo
+let getData =
+    Cookie.cookieState
+        cookieState
+        (fun _ -> Choice2Of2 <| RequestErrors.FORBIDDEN "")
+        (fun _ -> Choice2Of2 <| RequestErrors.FORBIDDEN "")
+        (context (fun ctx ->
+                    let x = ctx.userState.["userId"] :?> byte[] |> Encoding.UTF8.GetString
+                    OK <| sprintf "User-ID = %O" x))
 
 [<EntryPoint>]
 let main _ = 
-    startWebServer defaultConfig (GET >=> path "/1" >=> foo2 >=> foo)
-    startWebServer defaultConfig (GET >=> path "/2" >=> foo3)
+    let app =
+        choose [
+            GET >=> path "/" >=> OK "It works"
+            GET >=> pathScan "/login/%s" login >=> OK "OK"
+            GET >=> path "/data" >=> getData
+        ]
+    startWebServer defaultConfig app
     0
