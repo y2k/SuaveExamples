84cad775b4ccce66c83656ac41b5fabd4c36bf9a
y2k <itwisterlx@gmail.com>
2018-09-28 01:35:45 +0300

    handle web forms add docker

--- Program.fs
+++ Program.fs
@@ -1,12 +1,12 @@
-﻿open System
-open System.Text
-open Suave
-open Suave.Operators
-open Suave.Filters
-open Suave.Successful
+﻿open Suave
 open Suave.Cookie
-open Suave.Swagger.Swagger
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
 open Suave.Swagger.FunnyDsl
+open Suave.Swagger.Swagger
+open System
+open System.Text
 
 module Simple =
     let private cookieState =
@@ -29,20 +29,35 @@ module Simple =
             (fun _ -> Choice2Of2 <| RequestErrors.FORBIDDEN "")
             (fun _ -> Choice2Of2 <| RequestErrors.FORBIDDEN "")
             (context (fun ctx ->
-                        let x = ctx.userState.["userId"] :?> byte[] |> Encoding.UTF8.GetString
+                        let x = ctx.userState.["userId"] :?> byte [] |> Encoding.UTF8.GetString
                         OK <| sprintf "User-ID = %O" x))
-    let start () =
+    let loginViaGoogle =
+        request (fun r ->
+            r.formData "token"
+            |> Option.ofChoice
+            |> Option.map (fun x -> login x >=> OK "")
+            |> Option.defaultValue (RequestErrors.FORBIDDEN ""))
+    let time = 
+        request (fun _ -> OK(sprintf "Server time = %O\n" DateTime.Now))
+    let start() =
         choose [
+            GET >=> path "/time" >=> OK(sprintf "Server time = %O" DateTime.Now)
+            POST >=> path "/loginViaGoogle" >=> loginViaGoogle
             GET >=> path "/" >=> OK "It works"
             GET >=> pathScan "/login/%s" login >=> OK "OK"
             GET >=> path "/data" >=> getData
-        ] 
+        ]
         |> startWebServer defaultConfig
 
 module Swagger' =
     type TimeResult = TimeResult
-    let api = 
+    let api =
         swagger {
+            for route in getting <| simpleUrl "/time" |> thenReturns Simple.time do
+                yield route |> addResponse 200 "Return data" (Some typeof<string>)
+            for route in posting <| simpleUrl "/loginViaGoogle" |> thenReturns Simple.loginViaGoogle do
+                yield parameter "token" Of route
+                    (fun p -> { p with Type = (Some typeof<string>); In = FormData })
             for route in getting <| simpleUrl "/data" |> thenReturns Simple.getData do
                 yield route |> addResponse 200 "Return data" (Some typeof<string>)
                 yield route |> supportsJsonAndXml
@@ -53,12 +68,14 @@ module Swagger' =
                 yield route |> addResponse 200 "Login via email" None
                 yield route |> supportsJsonAndXml
         }
-    let start () =
-        startWebServer defaultConfig api.App
-
-
+    open System.Net
+    let start() =
+        let config = { defaultConfig with
+                           bindings = [ HttpBinding.create HTTP (IPAddress.Parse "0.0.0.0") 8080us ] }
+        startWebServer config api.App
 
 [<EntryPoint>]
-let main _ = 
-    Swagger'.start ()
+let main _ =
+    Swagger'.start()
+    // Simple.start ()
     0
